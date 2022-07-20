3ccfa77e6f5d8a6e89d6431161a94d21275d29b8
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2022-07-20 15:04:36 +0200

    stage function display Product

--- front/js/cart.js
+++ front/js/cart.js
@@ -9,8 +9,11 @@ const apiContent = [];
 
 //Décla variables
 let kanap_id;
-let kanapPrice;
+let kanap_img;
+let kanap_Price;
 let priceProduct;
+let kanap_Name;
+let kanap_Alt;
 
 /*main();
 function main(){
@@ -20,11 +23,10 @@ price();
 }*/
 
 //fonction qui récupère l' id et le prix via l' api
-function apiCall(){
- 
-    const kanap = fetch("http://localhost:3000/api/products");
- 
-    kanap.then((res) => {
+function apiCall(id){
+
+    const kanap = fetch("http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926");
+
         kanap.then((res, err)=>{
             
             if (res.ok){
@@ -32,23 +34,31 @@ function apiCall(){
                 let kanapPromise = res.json()
                 kanapPromise.then((kanapTable)=>{
                     
-                    for (let k= 0; k < kanapTable.length; k++){
-                        priceProduct = kanapTable[k]["price"];
+                   for (let k = 0; k < kanapTable.length; k++){
+                        console.log('dfkjvhqksudFHS')
+                        //priceProduct = kanapTable[k]["price"];
                         kanap_id = kanapTable[k]["_id"];
-                        kanap_img = kanapTable[k]["imgUrl"];    
+                        kanap_img = kanapTable[k]["imageUrl"]; 
+                        kanap_Alt =  kanapTable[k]["altTxt"];
+                        kanap_Name  = kanapTable[k]["name"];
+                        kanap_Price  = kanapTable["price"];
+                        console.log(kanap_Price);
 
                         tablePrice.push(priceProduct);
                         tableId.push(kanap_id);
-                        apiContent.push(kanapTable[k]["price"],kanapTable[k]["_id"]);
-                        //console.log(apiContent);
-        
-                    }
+                        apiContent.push(kanapTable[k]["price"],kanapTable[k]["_id"],kanapTable[k]["name"],kanapTable[k]["imageUrl"]);
+                        
+                    //}   console.log(apiContent);
+                        //console.log(priceProduct);
+                        //console.log(kanap_id);
+                        //console.log(kanap_Price);
+                        //console.log(kanap_Alt);
+                        //console.log(kanap_Name);
+                        console.log(kanap_Price);
                 })
             }  
 
-        }).catch((err)=>{
-            alert("erreur de chargement de la page")
-        })
+        
 
     }).catch((err)=>{
         alert("erreur de chargement de l' api")
@@ -57,77 +67,42 @@ function apiCall(){
 }
 apiCall();
 
-
 function displayProduct(){
-    
-    let selectedProduct = document.getElementById("cart__items");
-        if(productsFromLS == null){
-            console.log("erreur")
-            selectedProduct.innerHtml += "<h1>Votre panier est vide</h1>"
 
-        }else{
+    if (productsFromLS !== null) {
+        for (let p = 0; p < productsFromLS.length; p++){
+            console.log(productsFromLS[p].id)
+            apiCall(productsFromLS[p].id);
+            //console.log(productsFromLS[p].color);
+            //console.log(apiContent);
 
-            const selectedProduct = document.getElementById("cart__items");;
-            let kanap_id;        
-            let priceId;
-            const kanapTab = [];
+                cart__items.innerHTML += `<article class="cart__item" data-id="${productsFromLS[p].id}" data-color="${productsFromLS[p].color}">
+        <div class= "cart__item__img">
+          <img src="${apiContent[3]}" alt="">
+        </div>
+        <div class="cart__item__content">
+          <div class="cart__item__content__description">
+            <h2>${apiContent[2]}</h2>
+            <p>${productsFromLS[p].color}</p>
+            <p>${apiContent[4]} €</p>
+          </div>
+          <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+              <p>Qté :${productsFromLS[p].quantity} </p>
+              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="">
+            </div>
+            <div class="cart__item__content__settings__delete">
+              <p class="deleteItem">Supprimer</p>
+            </div>
+          </div>
+          </article>`
 
-                for(p = 0; p < productsFromLS.length; p++){
-                    kanap_id = productsFromLS[p]["_id"];
-                    priceId = productsFromLS[p].price;
-                    
-                 }
-                 
-                 console.log(productsFromLS);
-                 //Apel api pour verifier le prix
-                 let kanap = fetch("http://localhost:3000/api/products");
-                    kanap.then((response) => { 
-                        
-                        let kanapPromise = response.json();
-                        
-                        kanapPromise.then((kanapTable) => { 
-                            
-                            for (let k = 0; k < kanapTable.length; k++) {
-           
-                                if (kanap_id == kanapTable[k]['_id']){
-                                    priceId = kanapTable[k]['price']
-                                    
-                                }
-
-            }
-         
-             })
-                
-             })
-             
-             selectedProduct.innerHTML +=
-             `<article class="cart__item" data-id="${tableId}"data-color="${productsFromLS.color}">
-             <div class="cart__item__img">
-             <img src="${productsFromLS.imageUrl}" alt="Photographie d'un canapé">
-             </div>
-             <div class="cart__item__content">
-             <div class="cart__item__content__description">
-             <h2>${productsFromLS.name}</h2>
-             <p>${productsFromLS.description}</p>
-             <p>45 €</p>
-             </div>
-             <div class="cart__item__content__settings">
-             <div class="cart__item__content__settings__quantity">
-             <p>Qté : </p>
-             <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productsFromLS}">
-             </div>
-             <div class="cart__item__content__settings__delete">
-             <p class="deleteItem">Supprimer</p>
-             </div>
-             </div>
-             </div>
-             </article>` 
-             
-           
-
-      
-         }console.log(productsFromLS)
-       
+        }
+    } else {
+        cart__items.innerHTML += `<h2>Votre panier est vide</h2>`
+        alert("Le panier est vide!!!")
+    }
 };
+//apiCall();
 displayProduct();
 
